f228b720e931dd56c27a00753bdfbdc35e266b3b
trihuynhminh <trihuynhminh2006@gmail.com>
2025-10-01 23:33:24 +0700

    Add files via upload

---
+++ he.cpp
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+  int *p;
+  int n;
+  scanf("%d", &n);
+  p = new int[n];
+  for (int i = 0; i < n; i++) {
+    scanf("%d", &p[n - i - 1]);
+  }
+  for (int i = 0; i < n; i++) {
+    printf("%d ", p[i]);
+  }
+  return 0;
+}
